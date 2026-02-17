f0e64400e4060672c9ce58fc2a647fef965549bf
Romeiyo <romeopomeo1@gmail.com>
2026-02-17 13:07:45 +0200

    Started working on my booking form

---
+++ Conference-UI/src/components/BookingForm.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import Button from "./Button";
+
+function BookingForm({ onAddBooking }) {
+
+    const [roomName, setName] = useState("");
+    const [date, setDate] = useState(new Date());
+    const [startTime, setStartTime] = useState("");
+    const [endTime, setEndTime] = useState("");
+    const [location, setLocation] = useState("");
+    const [bookedBy, setBookedBy] = useState("");
+
+    const formatDateForInput = (dateObject) => {
+        const year = dateObject.getFullYear();
+        const month = (dateObject.getMonth() + 1).toString().padStart(2, '0');
+        const day = dateObject.getDate().toString().padStart(2, '0');
+        return `${year}-${month}-${day}`;
+    }
+
+    const handleStart = (event) => {
+        setStartTime(event.target.value);
+    } 
+
+    const handleEnd = (event) => {
+        setEndTime(event.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const newBooking = {
+            id: Date.now(),
+            roomName,
+            date: formatDateForInput(date),
+            startTime,
+            endTime,
+            location,
+            bookedBy
+        }
+
+        onAddBooking(newBooking);
+
+        // Clear form
+        setName("");
+        setDate(new Date());
+        setStartTime("");
+        setEndTime("");
+        setLocation("");
+        setBookedBy("");
+    };
+
+    return (
+        <form className="booking-form" onSubmit={handleSubmit}>
+            <input
+                type = "text"
+                placeholder="Room Name"
+                value={roomName}
+                onChange={(e) => setName(e.target.value)}
+            />
+        </form>
+    );
+}
